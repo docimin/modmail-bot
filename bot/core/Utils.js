576adfebbe86eb915cbf4bee5f6870bcf43727ce
Yui <dev@sleepyyui.com>
2023-01-26 19:26:50 +0100

    Fix 3 errors
    Reply with embedded link in ticket: Fixed
    
    /support command "Invalid Form Body" error: Fixed
    
    ECONNRESET: fixed? - trying to prevent by keeping the connection alive

--- bot/core/Utils.js
+++ bot/core/Utils.js
@@ -8,7 +8,9 @@ class Utils {
    finalString += message?.content;
 
   if (message?.embeds?.length > 0)
-   finalString += "\n" + this.getEmbedsAsString(message.embeds);
+   //finalString += "\n" + this.getEmbedsAsString(message.embeds);
+   // When links are sent and turn into an embed, the embeds array will contain the link as a string, which is the easiest way to save it
+   finalString += "\n" + message?.embeds?.url?.toString();
 
   if (message?.attachments?.size > 0)
    finalString += "\n" + message?.attachments?.map(a => a?.url)?.join(" ");
